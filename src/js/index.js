c9d62a1aeaf05ad49add6579187dd2eba8cbab32
Olga Gnatenko <lady.irony.ua@gmail.com>
2025-09-05 09:44:25 +0300

    fix: keep working logic only

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,2 @@
 import "../scss/main.scss";
 
-// component logic for Highlight section
-
-// handle thumbnails clicks 
-const highlightSections = document.getElementsByClassName('highlight');
-if (highlightSections && highlightSections.length > 0) {
-    const highlightSection = highlightSections[0];
-    highlightSection.addEventListener('click', (event) => {
-        const child = event.target.closest('.highlight__thumb-item');
-        if (child && highlightSection.contains(child)) {
-            const childImage = child.querySelector('img');
-            if (childImage & childImage.src) {
-                console.log(childImage.src);
-            }
-
-            const thumbs = highlightSection.querySelectorAll('.highlight__thumb-item');
-            thumbs.forEach((elem) => {
-                elem.classList.remove('active-item');
-            });            
-        }
-
-
-    })
-}
-
